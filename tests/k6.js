424b114eb1a306cfcd49f1783d3ceefbb61e1bd6
Jean-Cyril Bohy <jean-cyril.bohy@gologic.ca>
2022-10-11 16:30:32 -0400

    add load test

--- tests/k6.js
+++ tests/k6.js
@@ -3,10 +3,17 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 50,
-  };
+  stages: [
+    { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes
+    { duration: '5m', target: 0 }, // ramp-down to 0 users
+  ],
+  thresholds: {
+    'http_req_duration': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
 
 export default function () {
-  http.get('http://lab1.gologic.ca:5001/pi/500');
+  http.get('http://lab1.gologic.ca:5001/pi/50');
   //sleep(1);
 }
