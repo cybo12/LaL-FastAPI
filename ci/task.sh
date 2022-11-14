dd652e59702012caccc7895e80320d9f2a928fa1
Jean-Cyril Bohy <jean-cyril.bohy@gologic.ca>
2022-11-14 16:18:36 -0500

    test

--- ci/task.sh
+++ ci/task.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-pytho -m pip install --no-cache-dir --upgrade -r ../application/requirements.txt
+python -m pip install --no-cache-dir --upgrade -r ../application/requirements.txt
 python -m pytest
