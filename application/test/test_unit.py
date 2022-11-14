053433fd5725fb3f0d2d4d914e331213e254300f
Jean-Cyril Bohy <jean-cyril.bohy@gologic.ca>
2022-11-14 14:23:43 -0500

    test

--- application/test/test_unit.py
+++ application/test/test_unit.py
@@ -5,32 +5,15 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
-import re
-
-
-class pytest_regex:
-    """Assert that a given string meets some expectations."""
-
-    def __init__(self, pattern, flags=0):
-        self._regex = re.compile(pattern, flags)
-
-    def __eq__(self, actual):
-        return bool(self._regex.match(actual))
-
-    def __repr__(self):
-        return self._regex.pattern
-
-regex = pytest_regex('^[0-9a-fA-F]{12}\b')
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": regex ,"version":app.versionpi}
+    assert response.json()["version"] == app.version
 
-def test_pi_200():
-    response = client.get("/pi/200")
-    assert response.status_code == 200
-    assert response.json() == {"runtime: 0:00:00 or 0.02 seconds.",f"running version 0.1.0 on container {regex}"}
+#def test_pi_200():
+#    response = client.get("/pi/200")
+#    assert response.status_code == 200
 
 def test_pi_limit():
     response = client.get("/pi/50001")
